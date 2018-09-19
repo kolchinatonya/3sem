35e7491d94cf094826a75eff0eed26d8ee69b0f5
Kolchina Antonina <kolchina.aa@phystech.edu>
2018-09-19 13:33:13 +0300

    deedline 20.09

---
+++ hw01-abacaba.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int Count(int n)
+{
+    return (int)pow(2, n) - 1;
+}
+
+void GenerateString(int n, char* str)
+{
+    if (n < 1)
+        return;
+    str[0] = 'a';
+    for (int i = 1; i < n; ++i) {
+        char next_letter = (char)('a' + i);
+        str[Count(i)] = next_letter;
+        strncpy(str + Count(i) + 1, str, Count(i));
+        // Count(i) - длина строки на данной итерации
+    }
+    str[Count(n)] = '\0';
+}
+
+int main() {
+    int n, count;
+    scanf("%d", &n);
+    while (n != 0) {
+        count = Count(n);
+        char *str;
+        str = (char *) malloc(count * sizeof(char));
+        GenerateString(n, str);
+        printf("%s\n", str);
+        free(str);
+        scanf("%d", &n);
+    }
+    return 0;
+}

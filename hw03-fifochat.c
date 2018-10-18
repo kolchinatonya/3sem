0542e8a374770f8195ec44486f7d1ea591d85c54
Антонина Колчина <antoninakolcina@net122.ipoe-users.mipt.ru>
2018-10-18 09:48:41 +0300

    deadline 18.10.2018

---
+++ hw03-fifochat.c
@@ -0,0 +1,96 @@
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAX_STRING_SIZE 200
+
+
+int main(int argc, char *argv[])
+{
+    int to_read, to_write, fd;
+    pid_t result;
+    char* string;
+    int ind = atoi(argv[1]);
+    char* fifo[2];
+    if (ind == 0)
+    {
+        to_read = 0;
+        to_write = 1;
+    }
+    else
+    {
+        to_read = 1;
+        to_write = 0;
+    }
+
+    fifo[to_read] = "1.fifo";
+    fifo[to_write] = "2.fifo";
+
+    if ((mknod(fifo[0], S_IFIFO | 0666, 0) < 0) && (errno != EEXIST))
+    {
+        printf("Can\'t create FIFO\n");
+        exit(-1);
+    }
+
+
+    if ((mknod(fifo[1], S_IFIFO | 0666, 0) < 0) && (errno != EEXIST))
+    {
+
+        perror("i");
+        printf("Can\'t create FIFO\n");
+        exit(-1);
+    }
+
+    if((result = fork()) < 0)
+    {
+        exit(-1);
+    }
+    if (result > 0)
+    {
+        fd = open(fifo[1], O_WRONLY);
+        if (fd  == -1)
+        {
+            printf("Can\'t open FIFO for writting\n");
+            exit(-1);
+        }
+        while(1)
+        {
+            string = (char*)calloc(MAX_STRING_SIZE, sizeof(char));
+            fgets(string, MAX_STRING_SIZE, stdin);
+            write(fd, string, strlen(string));
+            free(string);
+        }
+        close(fd);
+
+    }
+    else
+    {
+        fd = open(fifo[0], O_RDONLY);
+        if (fd  == -1)
+        {
+            printf("Can\'t open FIFO for reading\n");
+            exit(-1);
+        }
+        while(1)
+        {
+            string = (char*)calloc(MAX_STRING_SIZE, sizeof(char));
+            int size = read(fd, string, MAX_STRING_SIZE);
+            if (size == 0)
+            {
+                printf("Another program exited\n");
+                free(string);
+                exit(0);
+            }
+            printf("- %s", string);
+            free(string);
+        }
+        close(fd);
+    }
+    return 0;
+}

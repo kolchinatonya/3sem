70d7da8dffebd7b0c76b9a5c870b574bc2a58bc7
Антонина Колчина <antoninakolcina@net90.ipoe-users.mipt.ru>
2018-10-26 20:35:33 +0300

    fixed

--- hw03-fifochat.c
+++ hw03-fifochat.c
@@ -13,46 +13,23 @@
 
 int main(int argc, char *argv[])
 {
-    int to_read, to_write, fd;
+    int fd;
     pid_t result;
     char* string;
     int ind = atoi(argv[1]);
     char* fifo[2];
-    if (ind == 0)
-    {
-        to_read = 0;
-        to_write = 1;
-    }
-    else
-    {
-        to_read = 1;
-        to_write = 0;
-    }
 
-    fifo[to_read] = "1.fifo";
-    fifo[to_write] = "2.fifo";
-
-    /// это можно было бы написать короче
-    /*
     fifo[ind] = "1.fifo";
     fifo[1 - ind] = "2.fifo";
-    */
     
-    // два последующие mknod одинаковые ... можно сделать цикл от 0 до 1
-    if ((mknod(fifo[0], S_IFIFO | 0666, 0) < 0) && (errno != EEXIST))
+    for (int i = 0; i < 2; ++i)
     {
-        printf("Can\'t create FIFO\n");
-        exit(-1);
-    }
-
-
-    if ((mknod(fifo[1], S_IFIFO | 0666, 0) < 0) && (errno != EEXIST))
-    {
-
-        perror("i");
-        printf("Can\'t create FIFO\n");
-        exit(-1);
-    }
+    	if ((mknod(fifo[i], S_IFIFO | 0666, 0) < 0) && (errno != EEXIST))
+    	{
+        	printf("Can\'t create FIFO\n");
+        	exit(-1);
+    	}
+	}
 
     if((result = fork()) < 0)
     {
@@ -66,15 +43,13 @@ int main(int argc, char *argv[])
             printf("Can\'t open FIFO for writting\n");
             exit(-1);
         }
+        string = (char*)calloc(MAX_STRING_SIZE, sizeof(char));
         while(1)
         {
-            // кажется лишним каждый раз в цикле выделять и освобождать память под строку.
-            // можно выделить память 1 раз до цикла
-            string = (char*)calloc(MAX_STRING_SIZE, sizeof(char));
             fgets(string, MAX_STRING_SIZE, stdin);
             write(fd, string, strlen(string));
-            free(string);
         }
+        free(string);
         close(fd);
 
     }
@@ -86,10 +61,9 @@ int main(int argc, char *argv[])
             printf("Can\'t open FIFO for reading\n");
             exit(-1);
         }
+        string = (char*)calloc(MAX_STRING_SIZE, sizeof(char));
         while(1)
         {
-            // аналогично память можно выделить 1 раз до цикла
-            string = (char*)calloc(MAX_STRING_SIZE, sizeof(char));
             int size = read(fd, string, MAX_STRING_SIZE);
             if (size == 0)
             {
@@ -98,8 +72,8 @@ int main(int argc, char *argv[])
                 exit(0);
             }
             printf("- %s", string);
-            free(string);
         }
+        free(string);
         close(fd);
     }
     return 0;

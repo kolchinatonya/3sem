ef5e33e1ec090606fcc6385ba5cc78aaed5dd745
Антонина Колчина <antoninakolcina@net90.ipoe-users.mipt.ru>
2018-12-15 03:01:45 +0300

    final realese

--- Sapper/Client.c
+++ Sapper/Client.c
@@ -1,16 +1,253 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<errno.h>
+#include<unistd.h>
+#include <stdbool.h>
 
 
 #define FIELD_SIZE 9
-#define MINE_PERCENT 25
 
-typedef struct Position
+typedef struct sockaddr_in Addr;
+
+typedef struct Cell
 {
     int x;
     int y;
-}Position;
+}Cell;
+
+typedef struct Inf
+{
+    Cell open;
+    int status;
+}Inf;
+
+int mine_count, opened_count = 0;
+
+void CheckIP(int argc);
+void Hello();
+void CreateNetConnection(char** IP, int* sockfd, Addr servaddr, Addr cliaddr);
+void GetFieldFromServer(int sockfd, int** server_field);
+void CountMines(int** server_field);
+char** GenerateEmptyField(int n);
+int** GenerateEmptyIntField(int n);
+void PrintIntField(int** field, int n);
+void Wait(int sockfd, int** server_field, char** my_field);
+void Play(int sockfd, int** server_field, char** my_field);
+int Action(Cell* open, int** server_field, char** my_field);
+int OpenCell(int** server_field, char** my_field, Cell* to_open, int val);
+void OpenAllCells(int** server_field, char** my_field);
+void PrintFieldWhithIndexes(char** field, int n);
+void ClearField(char** field, int n);
+void ClearIntField(int** field, int n);
+void GameOver(int** server_field, char** my_field, int sockfd);
+void Win(int** server_field, char** my_field, int sockfd);
+void Exit(int** server_field, char** my_field, int sockfd);
+
+
+int main(int argc, char **argv) {
+
+    CheckIP(argc);
+    Hello();
+
+    int sockfd;
+    Addr servaddr, cliaddr;
+
+    CreateNetConnection(argv, &sockfd, servaddr, cliaddr);
+
+    int **server_field = GenerateEmptyIntField(FIELD_SIZE);
+    char **my_field = GenerateEmptyField(FIELD_SIZE);
+
+    int my_turn;
+    recv(sockfd, &my_turn, sizeof(int), 0);
+    if (my_turn == 0) {
+        printf("Sorry! You are alone here :( Waiting for the second player...\n");
+        GetFieldFromServer(sockfd, server_field);
+        PrintFieldWhithIndexes(my_field, FIELD_SIZE);
+        Play(sockfd,server_field, my_field);
+    } else {
+        GetFieldFromServer(sockfd, server_field);
+        Wait(sockfd, server_field, my_field);
+        Play(sockfd,server_field, my_field);
+    }
+    return 0;
+}
+
+
+
+
+
+
+void CheckIP(int argc)
+{
+    if(argc != 2)
+    {
+        printf("Where is IP, man???? Bye! :p\n");
+        exit(-1);
+    }
+}
+
+void Hello()
+{
+    printf("Hello! Do you want to play Sapper? :) (y/n) \n");
+    char answer;
+    scanf("%c", &answer);
+    if (answer == 'n')
+    {
+        printf("\nOK! Bye-bye ;) \n");
+        exit(0);
+    }
+    if (answer == 'y')
+    {
+        return;
+    }
+    printf("\nYou are a stupid guy. I don't want to deal with you :( Bye!\n");
+    exit(0);
+}
+
+void CreateNetConnection(char** IP, int* sockfd, Addr  servaddr, Addr cliaddr)
+{
+    if ((*sockfd = socket (PF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("client: socket");
+        exit(1);
+    }
+    servaddr.sin_family	= AF_INET;
+    servaddr.sin_port = htons(51000);
+    if (inet_aton(IP[1], &servaddr.sin_addr) == -1)
+    {
+        perror("inet_aton");
+        exit(EXIT_FAILURE);
+    }
+    if (connect(*sockfd, (struct sockaddr*)&servaddr, sizeof(Addr)) == -1)
+    {
+        perror("client: connect");
+        close(*sockfd);
+        exit(1);
+    }
+}
+
+void GetFieldFromServer(int sockfd, int** server_field)
+{
+    for (int i = 0; i < FIELD_SIZE; ++i) {
+        for (int j = 0; j < FIELD_SIZE; ++j)
+            recv(sockfd, &server_field[i][j], sizeof(int), 0);
+    }
+    CountMines(server_field);
+}
+
+
+void CountMines(int** server_field)
+{
+    for (int i = 0; i < FIELD_SIZE; ++i) {
+        for (int j = 0; j < FIELD_SIZE; ++j) {
+            if (server_field[i][j] == 9)
+                mine_count++;
+        }
+    }
+
+}
+
+
+void Wait(int sockfd, int** server_field, char** my_field)
+{
+    system("clear");
+    PrintFieldWhithIndexes(my_field, FIELD_SIZE);
+    Inf inf;
+    printf("Please, wait for your turn.\n");
+    while (1)
+    {
+        recv(sockfd, &inf, sizeof(inf), 0);
+        printf("Status: %d\n", inf.status);
+        if (inf.status == 0)
+        {
+            int val = server_field[inf.open.x][inf.open.y];
+            OpenCell(server_field, my_field, &inf.open, val);
+            system("clear");
+            PrintFieldWhithIndexes(my_field, FIELD_SIZE);
+            printf("Please, wait for your turn.\n");
+            continue;
+        } else if (inf.status == 1)
+        {
+            int val = server_field[inf.open.x][inf.open.y];
+            OpenCell(server_field, my_field, &inf.open, val);
+            return;
+        }
+        else if (inf.status == -1)
+        {
+            OpenAllCells(server_field, my_field);
+            GameOver(server_field, my_field, sockfd);
+        } else
+        {
+            OpenAllCells(server_field, my_field);
+            Win(server_field, my_field, sockfd);
+        }
+    }
+
+}
+
+void Win(int** server_field, char** my_field, int sockfd)
+{
+    system("clear");
+    PrintFieldWhithIndexes(my_field, FIELD_SIZE);
+    printf("YOU WIN! :)\n");
+    Exit(server_field, my_field, sockfd);
+}
+
+void GameOver(int** server_field, char** my_field, int sockfd)
+{
+    system("clear");
+    PrintFieldWhithIndexes(my_field, FIELD_SIZE);
+    printf("YOU EXPLODED! :(\n");
+    Exit(server_field, my_field, sockfd);
+}
+
+void Exit(int** server_field, char** my_field, int sockfd)
+{
+    close(sockfd);
+    ClearIntField(server_field, FIELD_SIZE);
+    ClearField(my_field, FIELD_SIZE);
+    exit(0);
+}
+
+void Play(int sockfd, int** server_field, char** my_field) {
+    while(1) {
+        system("clear");
+        PrintFieldWhithIndexes(my_field, FIELD_SIZE);
+        printf("Put your command: (""F X Y"" or ""O X Y"")\n");
+        char command;
+        Cell cur;
+        scanf("%c %d %d", &command, &cur.x, &cur.y);
+        cur.x--;
+        cur.y--;
+        if (command == 'F')
+            my_field[cur.x][cur.y] = '%';
+        if (command == 'O')
+        {
+            int status = Action(&cur, server_field, my_field);
+            Inf inf = {cur, status};
+            send(sockfd, &inf, sizeof(inf), 0);
+            switch(status)
+            {
+                case -1:
+                    GameOver(server_field, my_field, sockfd);
+                case 0:
+                    continue;
+                case 1:
+                    Wait(sockfd, server_field, my_field);
+                    break;
+                default:
+                    OpenAllCells(server_field,my_field);
+                    Win(server_field, my_field, sockfd);
+            }
+        }
+    }
+}
 
 char** GenerateEmptyField(int n)
 {
@@ -24,6 +261,31 @@ char** GenerateEmptyField(int n)
     }
     return res_field;
 }
+
+int** GenerateEmptyIntField(int n)
+{
+    int** res_field = (int **)malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++)
+    {
+        res_field[i] = (int *)malloc(n * sizeof(int));
+        for (int j = 0; j < n; j++)
+        {
+            res_field[i][j] =  0;
+        }
+    }
+    return res_field;
+}
+
+void PrintIntField(int** field, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            printf("%2d ", field[i][j]);
+        printf("\n");
+    }
+}
+
 
 void PrintFieldWhithIndexes(char** field, int n)
 {
@@ -45,6 +307,29 @@ void PrintFieldWhithIndexes(char** field, int n)
     printf("\n");
 }
 
+int OpenCell(int** server_field, char** my_field, Cell* to_open, int val)
+{
+    int status = 0;
+    my_field[to_open->x][to_open->y] = val + '0';
+    opened_count++;
+    if (opened_count == FIELD_SIZE * FIELD_SIZE - mine_count)
+        status = 1;
+    return status;
+}
+
+void OpenAllCells(int** server_field, char** my_field) {
+    for (int i = 0; i < FIELD_SIZE; ++i) {
+        for (int j = 0; j < FIELD_SIZE; ++j) {
+            if (server_field[i][j] == 9)
+                my_field[i][j] = '@';
+            else {
+                int val = server_field[i][j];
+                Cell to_open = {i, j};
+                OpenCell(server_field, my_field, &to_open, val);
+            }
+        }
+    }
+}
 void ClearField(char** field, int n)
 {
     for (int i = 0; i < n; i++)
@@ -52,30 +337,34 @@ void ClearField(char** field, int n)
     free(field);
 }
 
-int main()
+void ClearIntField(int** field, int n)
 {
-    char** field = GenerateEmptyField(FIELD_SIZE);
-    int flag = 1;
-    while (flag)
+    for (int i = 0; i < n; i++)
+        free(field[i]);
+    free(field);
+}
+
+int Action(Cell* open, int** server_field, char** my_field)
+{
+    int val = server_field[open->x][open->y];
+    if ( val == 9)
     {
-        system ("clear");
-        PrintFieldWhithIndexes(field, FIELD_SIZE);
-        printf("Put your command: (""F X Y"" or ""O X Y"")\n");
-        char command;
-        Position cur;
-        scanf("%c %d %d", &command, &cur.x, &cur.y);
-        cur.x--;
-        cur.y--;
-        if (command == 'F')
-            field[cur.x][cur.y] = '%';
-        if (command == 'O')
-        {
-            field[cur.x][cur.y] = '0';
-            system ("clear");
-            flag = 0;
-            PrintFieldWhithIndexes(field, FIELD_SIZE);
-        }
+        OpenAllCells(server_field,my_field);
+        return -1;
     }
-    ClearField(field, FIELD_SIZE);
-    return 0;
+    else if (val == 0)
+    {
+        int status = OpenCell(server_field, my_field, open, val);
+        if (status == 1)
+            return 2;
+        return 0;
+    }
+    else
+    {
+        int status = OpenCell(server_field, my_field, open, val);
+        if (status == 1)
+            return 2;
+        return 1;
+    }
+
 }
--- Sapper/Server.c
+++ Sapper/Server.c
@@ -1,27 +1,89 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <assert.h>
+#include <float.h>
+#include <math.h>
+#include <limits.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <ncurses.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 #define FIELD_SIZE 9
 #define MINE_PERCENT 25
+#define MAX_QUEUE_SIZE 2
 
-typedef struct Position
+typedef struct Cell
 {
         int x;
         int y;
-}Position;
+}Cell;
+
+typedef struct Inf
+{
+    Cell open;
+    int status;
+}Inf;
+
+typedef struct sockaddr_in Addr;
+
+void SetAround(int** field, int n);
+int** GenerateField(int n);
+void PrintField(int** field, int n);
+void ClearField(int** field, int n);
+int AddClients(int* clients, int new_one);
+void AcceptClient(int* new_one, int* lsock);
+void CreateConnection(int* lsock, Addr* addr);
+void SendFieldToClient(int** field, int* clients);
+void PrepareForGame(int **field, int* clients, int* lsock);
+
+int main()
+{
+    int** field;
+    int clients[2] = {0, 0}, lsock = 0;
+    PrepareForGame(field, clients, &lsock);
+    Inf inf;
+    int cur_player = 0;
+    while (1)
+    {
+        recv(clients[cur_player], &inf, sizeof(inf), 0);
+        send(clients[!cur_player], &inf, sizeof(inf), 0);
+        if (inf.status == 1)
+            cur_player = !cur_player;
+        if ((inf.status == -1) || (inf.status == 2))
+        {
+            printf("THE GAME IS OVER!\n");
+            break;
+        }
+    }
+    close(clients[0]);
+    close(clients[1]);
+    close(lsock);
+    return 0;
+
+}
+
+
 
 void SetAround(int** field, int n)
 {
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
-            if (field[i][j] == 10)
+            if (field[i][j] == 9)
                 for (int k = i - 1; k < i + 2; ++k)
                     for (int l = j - 1; l < j + 2; ++l)
                     {
                         if ((k >= n) || (l >= n) || (k < 0) || (l < 0))
                             continue;
-                        if (field[k][l] != 10)
+                        if (field[k][l] != 9)
                             field[k][l] += 1;
                     }
 }
@@ -37,7 +99,7 @@ int** GenerateField(int n)
         for (int j = 0; j < n; j++)
         {
             int random = rand() % 101;
-            res_field[i][j] = (random < MINE_PERCENT) ? 10 : 0;
+            res_field[i][j] = (random < MINE_PERCENT) ? 9 : 0;
         }
     }
     SetAround(res_field, n);
@@ -61,12 +123,95 @@ void ClearField(int** field, int n)
     free(field);
 }
 
-
-
-int main()
+int AddClients(int* clients, int new_one)
 {
-    int** field = GenerateField(FIELD_SIZE);
-    PrintField(field, FIELD_SIZE);
-    ClearField(field, FIELD_SIZE);
-    return 0;
+    if	(!clients[0])
+    {
+        clients[0] = new_one;
+        return 1;
+    }
+    else if (!clients[1])
+    {
+        clients[1] = new_one;
+        return 2;
+    }
+    else
+        return 0;
 }
+
+void AcceptClient(int* new_one, int* lsock)
+{
+    if ((*new_one = accept(*lsock, NULL, NULL)) == -1)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+
+
+void CreateConnection(int* lsock, Addr* addr)
+{
+    *lsock = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(*lsock, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+    if (*lsock == -1)
+    {
+        perror("server: socket");
+        exit(EXIT_FAILURE);
+    }
+
+    addr->sin_family = AF_INET;
+    addr->sin_port = htons(51000);
+    addr->sin_addr.s_addr = htons(INADDR_ANY);
+    if (bind(*lsock, (struct sockaddr*) addr, sizeof(*addr)) == -1)
+    {
+        perror("server: bind");
+        exit(EXIT_FAILURE);
+    }
+    listen(*lsock, MAX_QUEUE_SIZE);
+}
+
+void SendFieldToClient(int** field, int* clients)
+{
+    for (int k = 0; k < 2; ++k) {
+        for (int i = 0; i < FIELD_SIZE; ++i) {
+            for (int j = 0; j < FIELD_SIZE; ++j)
+                send(clients[k], &field[i][j], sizeof(int), 0);
+        }
+    }
+}
+
+
+void PrepareForGame(int **field, int* clients, int* lsock)
+{
+    field = GenerateField(FIELD_SIZE);
+    PrintField(field, FIELD_SIZE);
+
+    int  new_one = 0;
+    Addr addr = {};
+    CreateConnection(lsock, &addr);
+
+    while (1)
+    {
+        AcceptClient(&new_one, lsock);
+        int clients_count = AddClients(clients, new_one);
+        if (clients_count == 1)
+        {
+            int turn = 0;
+            send(clients[0], &turn, sizeof(int), 0);
+        }
+        if (clients_count == 2)
+        {
+            int turn = 1;
+            send(clients[1], &turn, sizeof(int), 0);
+            SendFieldToClient(field,clients);
+            return;
+        }
+    }
+
+}
+
+
+
+
